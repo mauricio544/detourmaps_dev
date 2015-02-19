d4cbf343cbdbd4f62ac99d033a60d3240672ab81
devmau <mauri544@about.me>
2015-02-19 18:02:01 -0500

    Manejo de templates y vistas con angular, para una mejor carga de la vista del negocio.

--- mariustest/urls.py
+++ mariustest/urls.py
@@ -3,6 +3,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns('mariustest.views',
 	url(r'^$', 'single'),
+    url(r'^deals/$', 'deals', name='deals'),
+    url(r'^business/$', 'business', name='business'),
 )
 
 urlpatterns += staticfiles_urlpatterns()
--- mariustest/views.py
+++ mariustest/views.py
@@ -6,3 +6,11 @@ from django.template import RequestContext
 # Create your views here.
 def single(request):
     return render_to_response('community/index.html', {}, context_instance=RequestContext(request))
+
+
+def deals(request):
+    return render_to_response('community/deals.html', {}, context_instance=RequestContext(request))
+
+
+def business(request):
+    return render_to_response('community/business.html', {}, context_instance=RequestContext(request))
